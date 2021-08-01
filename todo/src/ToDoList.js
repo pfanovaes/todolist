0d6f6280c10c6e39e3fbd91c355daf0ac055e5e9
Patricia Novaes <patricia.novaes@gmail.com>
2021-08-01 01:05:00 -0300

    Create addItem function

--- todo/src/ToDoList.js
+++ todo/src/ToDoList.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+        this.addItem = this.addItem.bind(this);
+        
+        this.state = {
+            Item:[]
+        };
+    }
+
+    addItem(e) {
+        if (this._inputElement.value !== "") {
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+
+            };
+
+            this.setState((previouslyState) => {
+                return {
+                    items: previouslyState.items.concat(newItem)
+                };
+            });
+
+            this._inputElement.value = "";
+        }
+
+        e.preventDefault();
+    }
+
+    
+
+
+    
+
     render() {
         return (
             <>
-                <input placeholder= "Digite aqui sua tarefa" />
+                <form onSubmit={ this.addItem }>
+                <input ref={ (e) => this.inputElement = e} placeholder= "Digite aqui sua tarefa" />
+                <button type="submit">Enviar</button>
+                </form>
             </>
         );
 
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import "./style.css";
 import ToDoList from "./ToDoList.js";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <ToDoList />
+      <ToDoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
